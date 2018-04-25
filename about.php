759531694786b1f37c53f6428a9fc357271c43a4
Belal Khan <34804073+belalcse@users.noreply.github.com>
2018-04-25 13:26:25 +0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, aspernatur.</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti excepturi natus at, ipsa assumenda, nisi earum officia cum consectetur vero enim, blanditiis perspiciatis reprehenderit iure quam quia libero? Nisi, voluptatum.</p>
+</body>
+</html>
